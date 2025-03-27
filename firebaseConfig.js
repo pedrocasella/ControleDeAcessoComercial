e6f58ce7afe96db61c88e22c950fca80cfc9cfa6
Pedro Casella <pedrocasella27@gmail.com>
2025-03-27 17:17:09 -0300

    atualização no método de armazenamento de imagem

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyAA4UiqfIpy-7Subp2hM29z88Ff-5xW84Y",
+    authDomain: "teste-iestudei-ia.firebaseapp.com",
+    projectId: "teste-iestudei-ia",
+    storageBucket: "teste-iestudei-ia.firebasestorage.app",
+    messagingSenderId: "980198856740",
+    appId: "1:980198856740:web:944eccb736499c05bfe0b1"
+  };
